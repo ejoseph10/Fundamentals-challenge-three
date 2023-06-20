6d32af1c59db799733db58542962dc8c1a7c5eb0
ejoseph10 <emanny1110@gmail.com>
2023-06-20 10:55:11 -0400

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,71 @@
 // Assignment code here
 
-
-// Get references to the #generate element
+// Queries Section
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
+
+// Variables Section
+var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var optionsVariable = "";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // Password field reset
+    password.value === "";
+    var randomString = "";
+   
+    // Screen refresh blocker
+    event.preventDefault();
+   
+    // Password choice prompts
+    var lowercaseLett = confirm("Would you like your password to contain lowercase letters?");
+    var uppercaseLett = confirm("Would you like your password to contain uppercase letters?");
+    var num = confirm("Would you like your password to contain numbers?");
+    var specialChar = confirm("Would you like your password to contain special characters?");
+    var passwordLength = prompt("Password length must be at least 8 characters and no more than 128 characters.");
 
-  passwordText.value = password;
+    // Statement monitor alert
+    if (passwordLength <8 || passwordLength >128) {
+        alert("Password is not between 8 characters and no more than 128 characters. Please try again.");
+        var passwordLength = prompt("Password length must be at least 8 characters and no more than 128 characters.");
+    }
 
+ // Conditional choice prompts
+ else if (lowercaseLett === false && uppercaseLett === false && num === false && specialChar === false) {
+  alert("You must chose at least one password criteria.");
+  var lowercaseLett = confirm("Do you want your password to contain lowercase letters?");
+  var uppercaseLett = confirm("Do you want your password to contain uppercase letters?");
+  var num = confirm("Do you want your password to contain numbers?");
+  var specialChar = confirm("Do you want your password to contain special characters?");
+}
+
+// Random choice influenced generator
+for (var i = 0; i < passwordLength; i++) {
+  // Continuous character length generator
+  randomString += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+}
+
+// Choice based password generator
+if (lowercaseLett) {
+  optionsVariable += lowercaseAlphabet;
+}
+
+if (uppercaseLett) {
+  optionsVariable += uppercaseAlphabet;
+}
+
+if (num) {
+  optionsVariable += numbers;
+}
+
+if (specialChar) {
+  optionsVariable += specialCharacters;
+}
+
+password.value = randomString;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Password Generator Starter Code
+
+# Summary
+
+The Module 3 assignment was to make a functional password generator
+
+- HTML & CSS were given and remained untouched
+  User Criteria fulfilled - password generator now functions properly;
+
+GIVEN a user need a new, secure password
+WHEN a user clicks the button to generate a password
+the user is presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+the user selects which criteria to include in the password
+WHEN prompted for the length of the password
+the user chooses a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+the user chooses lowercase, uppercase, numeric, and/or special characters
+WHEN the user answers each prompt
+the user's input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+a password is generated that matches the selected criteria
+WHEN the password is generated
+the password is either displayed in an alert or written to the page
+
+![Debugged password generator screenshot](images/passgen_debugged.png)
